ab0579d24d752d3c941133dc2be167fd26bf069a
jlyon1 <joeylyon1@gmail.com>
2017-10-01 22:39:53 -0400

    updated file server location

--- main.go
+++ main.go
@@ -9,11 +9,10 @@ import (
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
-
 func main() {
 
 	mysocket.Init();
-  fs := http.FileServer(http.Dir("static"))
+  fs := http.FileServer(http.Dir("./static/"))
 
   http.Handle("/", fs)
 	http.HandleFunc("/ws", mysocket.ReceiveClient)
